9547180e910c3619626caf364e15bd63e9c89ad8
Te <linkin_pong_208@hotmail.com>
2020-01-29 17:52:23 +0700

    both

--- allapi.php
+++ allapi.php
@@ -4,7 +4,8 @@ header("Content-Type:application/json");
 $connection = DB();
 
 
-$sql = "SELECT * from users";
+// select all and search
+$sql = "SELECT * from users WHERE Firstname LIKE '%". $_GET['search']."%' ";
 $rows = array();
 $result = mysqli_query($connection, $sql);
 
@@ -27,44 +28,45 @@ echo json_encode($a);
 
 
 // SEARCH 
-// $search = $_GET('search');
+
 // if (isset($_GET['search'])){
 //     $sql = "SELECT * from users WHERE Firstname LIKE \'%'.$_GET['search'].'%\'
 //             OR Lastname LIKE \'%'.$_GET['search'].'%\'" ;
 // }
 
 // output gonna response to index.php 
-$output = '';
-$sql1 = "SELECT * from users WHERE id LIKE '%". $_GET['search']."%' ";
-$result1 = mysqli_query($connection, $sql1);
+// $output = '';
+// $sql1 = "SELECT * from users WHERE id LIKE '%". $_GET['search']."%' ";
 
-//  
-$output.="<h4 align='center'>Search Result</h4>";
-if(mysqli_num_rows($result1)>0){
-$output.="<table class='table table-bordered'>
-    <tr>
-        <th>ID</th>
-        <th>Firstname</th>
-        <th>Lastname</th>
-        <th>Email</th>
-        <th>MobileNo</th>
-        <th>Address</th>
-    </tr>";
-    while ($row = $result1->fetch_assoc()) {
-            $output.="<tr>
-            <td>".$row["id"]."</td>
-            <td>".$row["Firstname"]."</td>
-            <td>".$row["Lastname"]."</td>
-            <td>".$row["Email"]."</td>
-            <td>".$row["MobileNo"]."</td>
-            <td>".$row["Address"]."</td>
-            </tr>";
-    }
-    $output.="</table>";
-    echo $output;
-}else{
-    echo "No result Check ur code";
-}
+// $result1 = mysqli_query($connection, $sql1);
+
+// //  
+// $output.="<h4 align='center'>Search Result</h4>";
+// if(mysqli_num_rows($result1)>0){
+// $output.="<table class='table table-bordered'>
+//     <tr>
+//         <th>ID</th>
+//         <th>Firstname</th>
+//         <th>Lastname</th>
+//         <th>Email</th>
+//         <th>MobileNo</th>
+//         <th>Address</th>
+//     </tr>";
+//     while ($row = $result1->fetch_assoc()) {
+//             $output.="<tr>
+//             <td>".$row["id"]."</td>
+//             <td>".$row["Firstname"]."</td>
+//             <td>".$row["Lastname"]."</td>
+//             <td>".$row["Email"]."</td>
+//             <td>".$row["MobileNo"]."</td>
+//             <td>".$row["Address"]."</td>
+//             </tr>";
+//     }
+//     $output.="</table>";
+//     echo $output;
+// }else{
+//     echo "No result Check ur code";
+// }
 
 
 
